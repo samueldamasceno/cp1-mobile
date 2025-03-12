516115c598105b1994faf2c459b13adc195913ff
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-03-12 08:54:23 -0300

    campos de formulário adicionados com useState

--- App.js
+++ App.js
@@ -1,24 +1,42 @@
 import React, { useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
-
-  const [valor, setvalor] = useState(0)
-  const [porcentagem, setporcentagem] = useState(0)
-
-  function clicado() {
-    console.log("O botão foi clicado")
-  }
+  const [nomeProduto, setNomeProduto] = useState('');
+  const [valorOriginal, setValorOriginal] = useState(0);
+  const [porcentagemAumento, setPorcentagemAumento] = useState(0);
+  const [resultado, setResultado] = useState(null);
 
   return (
-    <View>
-      <TextInput style={{ margin: 30, fontSize: 30 }}
-        placeholder='Digite seu valor'
-        value={valor}
-        onChangeText={(valor) => setValor(valor)}
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do produto"
+        value={nomeProduto}
+        onChangeText={setNomeProduto}
       />
-      <Text Text>Resultado: {valor}</Text>
-      <Button title='Clique aqui' onPress={() => { console.log("Clicando ARROW") }} />
+      <TextInput
+        style={styles.input}
+        placeholder="Valor original"
+        value={valorOriginal}
+        onChangeText={setValorOriginal}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Porcentagem de aumento"
+        value={porcentagemAumento}
+        onChangeText={setPorcentagemAumento}
+      />
+      <Button title="Calcular" />
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+});
