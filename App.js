ff4fb495495190f321e8bc97b638933a0b559ea8
samueldamasceno <samueldamasceno.vip@gmail.com>
2025-03-12 08:52:27 -0300

    estrutura inicial

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [valor, setvalor] = useState(0)
+  const [porcentagem, setporcentagem] = useState(0)
+
+  function clicado() {
+    console.log("O botão foi clicado")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <TextInput style={{ margin: 30, fontSize: 30 }}
+        placeholder='Digite seu valor'
+        value={valor}
+        onChangeText={(valor) => setValor(valor)}
+      />
+      <Text Text>Resultado: {valor}</Text>
+      <Button title='Clique aqui' onPress={() => { console.log("Clicando ARROW") }} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
